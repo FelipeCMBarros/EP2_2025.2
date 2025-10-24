e2d0c4591fac709417ee025e87a4fb306dc2833b
jpcanato <joaopcc1@al.insper.edu.br>
2025-10-24 15:13:39 -0300

    Criação da função que armazena as posições dos navios

--- funcoes.py
+++ funcoes.py
@@ -11,4 +11,12 @@ def define_posicoes(linha,coluna,orientacao,tamanho):
             posicoes.append([linha, coluna + i])
             i += 1
     return posicoes
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio not in frota:
+        frota[nome_navio] = [posicoes]
+    else:
+        frota[nome_navio].append(posicoes)
+    return frota
+
         
